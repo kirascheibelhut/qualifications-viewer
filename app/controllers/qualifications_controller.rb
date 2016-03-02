c744bc4007841a09857c90e6983144d4cead9619
kirascheibelhut <kirascheibelhut@gmail.com>
2016-03-01 20:48:51 -0500

    Add show action to qualifications controller

--- app/controllers/qualifications_controller.rb
+++ app/controllers/qualifications_controller.rb
@@ -2,4 +2,9 @@ class QualificationsController < ApplicationController
   def index
     @qualifications = Qualification.all
   end
+
+  def show
+    @qualification = Qualification.find(params[:id])
+    @subjects = @qualification.subjects
+  end
 end
