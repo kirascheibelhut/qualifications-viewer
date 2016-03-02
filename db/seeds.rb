ca2eac0418116f66ce256110aacd7774824cd6b7
kirascheibelhut <kirascheibelhut@gmail.com>
2016-03-01 19:26:13 -0500

    Create records to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Creates qualification and subject records for database from parsed JSON feed.
+
+# Parses JSON feed into Ruby hash.
+URL = "https://api.gojimo.net/api/v4/qualifications"
+response = HTTParty.get(URL)
+qualifications = response.parsed_response
+
+qualifications.each do |qualification|
+  qual = Qualification.create(name: qualification["name"], country: qualification.dig("country","name"))
+  qualification["subjects"].each do |subject|
+    Subject.create(name: subject["title"], colour: subject["colour"], qualification_id: qual.id)
+  end
+end
