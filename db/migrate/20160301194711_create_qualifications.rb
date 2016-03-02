fbc503355d20192a0e80ca76861865d46aeb1514
kirascheibelhut <kirascheibelhut@gmail.com>
2016-03-01 19:30:39 -0500

    Modify migration files

--- db/migrate/20160301194711_create_qualifications.rb
+++ db/migrate/20160301194711_create_qualifications.rb
@@ -1,6 +1,8 @@
 class CreateQualifications < ActiveRecord::Migration
   def change
     create_table :qualifications do |t|
+      t.string :name
+      t.string :country
 
       t.timestamps null: false
     end
--- db/migrate/20160301194747_create_subjects.rb
+++ db/migrate/20160301194747_create_subjects.rb
@@ -1,6 +1,9 @@
 class CreateSubjects < ActiveRecord::Migration
   def change
     create_table :subjects do |t|
+      t.string :name
+      t.string :colour
+      t.references :qualification
 
       t.timestamps null: false
     end
